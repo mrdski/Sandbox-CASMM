fcf584f8e44295d9a369be6a78ab27ffb6e15604
MateusSalavC <mateus.salav@gmail.com>
2023-11-25 18:28:45 -0500

    the block definition is relatively working

--- client/src/components/ActivityPanels/BlocklyCanvasPanel/canvas/blockFactory/CustomBlock.jsx
+++ client/src/components/ActivityPanels/BlocklyCanvasPanel/canvas/blockFactory/CustomBlock.jsx
@@ -19,6 +19,7 @@ import PublicCanvas from '../PublicCanvas';
 import './blocks';
 
 
+
 let plotId = 1;
 
 export default function CustomBlock({ activity, isSandbox, workspace}) {
@@ -46,16 +47,11 @@ export default function CustomBlock({ activity, isSandbox, workspace}) {
   const activityRef = useRef(null);
 
 
-  // const xmlToBlockDefinition = (xmlText) => {
-
-  // };
-  
-  
-
   const setWorkspace = () => {
     workspaceRef.current = window.Blockly.inject('newblockly-canvas', {
       toolbox: document.getElementById('toolbox'),
     });
+  
     // Define the XML for the root block
     const rootBlockXml = '<xml>' +
       '<block type="factory_base" deletable="false" movable="false"></block>' +
@@ -67,15 +63,21 @@ export default function CustomBlock({ activity, isSandbox, workspace}) {
     // Initialize the workspace with the root block
     Blockly.Xml.domToWorkspace(xmlDom, workspaceRef.current);
   
-    workspaceRef.current.addChangeListener(() => {
-      const xml = Blockly.Xml.workspaceToDom(workspaceRef.current);
-      const xmlText = Blockly.Xml.domToText(xml);
-      setBlockCode(xmlText);
-  
-      const generatorCode = Blockly.JavaScript.workspaceToCode(workspaceRef.current);
-      setGeneratorCode(generatorCode);
+    // Event listener for block creation
+    workspaceRef.current.addChangeListener((event) => {
+      if (event.type === 'create' || event.type == 'change') {
+        // A new block is created
+        const xml = Blockly.Xml.workspaceToDom(workspaceRef.current);
+        const xmlText = Blockly.Xml.domToText(xml);
+        setBlockCode(xmlText);
+        const genCode = updateLanguage(xmlText);
+        //const genCode = updateLanguage();
+        setGeneratorCode(genCode);
+      }
+      
     });
   };
+  
 
 
   //Testing
@@ -84,15 +86,19 @@ export default function CustomBlock({ activity, isSandbox, workspace}) {
     // }, [workspace]);
 
 
-  useEffect(() => {
-    const setUp = async () => {
-      activityRef.current = activity;
-      if (!workspaceRef.current && activity && Object.keys(activity).length !== 0) {
-        setWorkspace();
-      }
-    };
-    setUp();
-  }, [activity]);
+  
+
+
+    useEffect(() => {
+      const setUp = async () => {
+        activityRef.current = activity;
+        if (!workspaceRef.current && activity && Object.keys(activity).length !== 0) {
+          setWorkspace();
+        }
+      };
+      setUp();
+    }, [activity]);
+  
   
   const handleUndo = () => {
     if (workspaceRef.current.undoStack_.length > 0)
@@ -239,6 +245,331 @@ export default function CustomBlock({ activity, isSandbox, workspace}) {
     </button>
   );
 
+/**
+ * Convert XML code to Blockly JavaScript code.
+ * @param {string} xmlCode - The input XML code.
+ * @returns {string} - The generated Blockly JavaScript code.
+ */
+function xmlToBlocklyJs(xmlCode) {
+  // Parse the XML code into a DOM structure.
+  
+  var xmlDoc = new DOMParser().parseFromString(xmlCode, 'text/xml');
+
+  // Helper function to process a block and its children.
+  function parseBlock(block) {
+
+    var blockType = block.getAttribute('type') || 'unnamed';
+    var jsCode = "Blockly.Blocks['" + blockType + "'] = {\n";
+    jsCode += "  init: function() {\n";
+
+    // Process fields.
+    var fields = block.querySelectorAll('field');
+    if (fields.length > 0) {
+      fields.forEach(function (field) {
+        jsCode += "    this.appendField('" + field.textContent + "');\n";
+      });
+    }
+
+    // Process inputs.
+    var inputs = block.querySelectorAll('value, statement, shadow');
+    if (inputs.length > 0) {
+      inputs.forEach(function (input) {
+        var inputName = input.getAttribute('name');
+        jsCode += "    this.append" + input.tagName + "('" + inputName + "', " +
+          parseBlock(input.firstElementChild) + ");\n";
+      });
+    }
+
+    // Process mutations.
+    var mutation = block.querySelector('mutation');
+    if (mutation) {
+      for (var i = 0; i < mutation.attributes.length; i++) {
+        var attribute = mutation.attributes[i];
+        jsCode += "    this.setMutatorAttribute('" + attribute.name + "', '" +
+          attribute.value + "');\n";
+      }
+    }
+
+    jsCode += "  }\n";
+    jsCode += "};\n\n";
+
+    return jsCode;
+  }
+
+  // Get the root block and start parsing.
+  var rootBlock = xmlDoc.querySelector('block[type="factory_base"]');
+  if (rootBlock) {
+    return parseBlock(rootBlock);
+  } else {
+    return ''; // Return an empty string if no block is found.
+  }
+}
+
+function updateLanguage(xmlCode, varToChange) {
+  var xmlDoc = new DOMParser().parseFromString(xmlCode, 'text/xml');
+  var blockX = xmlDoc.querySelector('block[type="factory_base"]');
+  var temporaryWorkspace = new Blockly.Workspace();
+  var rootBlock = Blockly.Xml.domToBlock(blockX, temporaryWorkspace);
+  if (!rootBlock) {
+    return "block not found";
+  }
+  var blockType = rootBlock.getFieldValue('NAME');
+  if (!blockType) {
+    blockType = UNNAMED;
+  }
+  blockType = blockType.replace(/\W/g, '_').replace(/^(\d)/, '_\\1');
+
+  varToChange = formatJson_(blockType, rootBlock);
+  //var code = blockType;
+  temporaryWorkspace.clear();  
+  //injectCode(code, 'blocklyCanvasMid');
+  
+  //updatePreview();
+}
+
+
+function formatJson_(blockType, rootBlock) {
+  var JS = {};
+  // Type is not used by Blockly, but may be used by a loader.
+  JS.type = blockType;
+  // Generate inputs.
+  var message = [];
+  var args = [];
+  var contentsBlock = rootBlock.getInputTargetBlock('INPUTS');
+  var lastInput = null;
+  while (contentsBlock) {
+    if (!contentsBlock.disabled && !contentsBlock.getInheritedDisabled()) {
+      var fields = getFieldsJson_(contentsBlock.getInputTargetBlock('FIELDS'));
+      for (var i = 0; i < fields.length; i++) {
+        if (typeof fields[i] == 'string') {
+          message.push(fields[i].replace(/%/g, '%%'));
+        } else {
+          args.push(fields[i]);
+          message.push('%' + args.length);
+        }
+      }
+
+      var input = {type: contentsBlock.type};
+      // Dummy inputs don't have names.  Other inputs do.
+      if (contentsBlock.type != 'input_dummy') {
+        input.name = contentsBlock.getFieldValue('INPUTNAME');
+      }
+      var check = JSON.parse(getOptTypesFrom(contentsBlock, 'TYPE') || 'null');
+      if (check) {
+        input.check = check;
+      }
+      var align = contentsBlock.getFieldValue('ALIGN');
+      if (align != 'LEFT') {
+        input.align = align;
+      }
+      args.push(input);
+      message.push('%' + args.length);
+      lastInput = contentsBlock;
+    }
+    contentsBlock = contentsBlock.nextConnection &&
+        contentsBlock.nextConnection.targetBlock();
+  }
+  // Remove last input if dummy and not empty.
+  if (lastInput && lastInput.type == 'input_dummy') {
+    var fields = lastInput.getInputTargetBlock('FIELDS');
+    if (fields && getFieldsJson_(fields).join('').trim() != '') {
+      var align = lastInput.getFieldValue('ALIGN');
+      if (align != 'LEFT') {
+        JS.lastDummyAlign0 = align;
+      }
+      args.pop();
+      message.pop();
+    }
+  }
+  JS.message0 = message.join(' ');
+  if (args.length) {
+    JS.args0 = args;
+  }
+  // Generate inline/external switch.
+  if (rootBlock.getFieldValue('INLINE') == 'EXT') {
+    JS.inputsInline = false;
+  } else if (rootBlock.getFieldValue('INLINE') == 'INT') {
+    JS.inputsInline = true;
+  }
+  // Generate output, or next/previous connections.
+  switch (rootBlock.getFieldValue('CONNECTIONS')) {
+    case 'LEFT':
+      JS.output =
+          JSON.parse(getOptTypesFrom(rootBlock, 'OUTPUTTYPE') || 'null');
+      break;
+    case 'BOTH':
+      JS.previousStatement =
+          JSON.parse(getOptTypesFrom(rootBlock, 'TOPTYPE') || 'null');
+      JS.nextStatement =
+          JSON.parse(getOptTypesFrom(rootBlock, 'BOTTOMTYPE') || 'null');
+      break;
+    case 'TOP':
+      JS.previousStatement =
+          JSON.parse(getOptTypesFrom(rootBlock, 'TOPTYPE') || 'null');
+      break;
+    case 'BOTTOM':
+      JS.nextStatement =
+          JSON.parse(getOptTypesFrom(rootBlock, 'BOTTOMTYPE') || 'null');
+      break;
+  }
+  // Generate colour.
+  var colourBlock = rootBlock.getInputTargetBlock('COLOUR');
+  if (colourBlock && !colourBlock.disabled) {
+    var hue = parseInt(colourBlock.getFieldValue('HUE'), 10);
+    JS.colour = hue;
+  }
+  JS.tooltip = '';
+  JS.helpUrl = 'http://www.example.com/';
+  return JSON.stringify(JS, null, '  ');
+}
+
+function getFieldsJson_(block) {
+  var fields = [];
+  while (block) {
+    if (!block.disabled && !block.getInheritedDisabled()) {
+      switch (block.type) {
+        case 'field_static':
+          // Result: 'hello'
+          fields.push(block.getFieldValue('TEXT'));
+          break;
+        case 'field_input':
+          fields.push({
+            type: block.type,
+            name: block.getFieldValue('FIELDNAME'),
+            text: block.getFieldValue('TEXT')
+          });
+          break;
+        case 'field_angle':
+          fields.push({
+            type: block.type,
+            name: block.getFieldValue('FIELDNAME'),
+            angle: Number(block.getFieldValue('ANGLE'))
+          });
+          break;
+        case 'field_checkbox':
+          fields.push({
+            type: block.type,
+            name: block.getFieldValue('FIELDNAME'),
+            checked: block.getFieldValue('CHECKED') == 'TRUE'
+          });
+          break;
+        case 'field_colour':
+          fields.push({
+            type: block.type,
+            name: block.getFieldValue('FIELDNAME'),
+            colour: block.getFieldValue('COLOUR')
+          });
+          break;
+        case 'field_date':
+          fields.push({
+            type: block.type,
+            name: block.getFieldValue('FIELDNAME'),
+            date: block.getFieldValue('DATE')
+          });
+          break;
+        case 'field_variable':
+          fields.push({
+            type: block.type,
+            name: block.getFieldValue('FIELDNAME'),
+            variable: block.getFieldValue('TEXT') || null
+          });
+          break;
+        case 'field_dropdown':
+          var options = [];
+          for (var i = 0; i < block.optionCount_; i++) {
+            options[i] = [block.getFieldValue('USER' + i),
+                block.getFieldValue('CPU' + i)];
+          }
+          if (options.length) {
+            fields.push({
+              type: block.type,
+              name: block.getFieldValue('FIELDNAME'),
+              options: options
+            });
+          }
+          break;
+        case 'field_image':
+          fields.push({
+            type: block.type,
+            src: block.getFieldValue('SRC'),
+            width: Number(block.getFieldValue('WIDTH')),
+            height: Number(block.getFieldValue('HEIGHT')),
+            alt: block.getFieldValue('ALT')
+          });
+          break;
+      }
+    }
+    block = block.nextConnection && block.nextConnection.targetBlock();
+  }
+  return fields;
+}
+
+function escapeString(string) {
+  return JSON.stringify(string);
+}
+
+function getOptTypesFrom(block, name) {
+  var types = getTypesFrom_(block, name);
+  if (types.length == 0) {
+    return undefined;
+  } else if (types.indexOf('null') != -1) {
+    return 'null';
+  } else if (types.length == 1) {
+    return types[0];
+  } else {
+    return '[' + types.join(', ') + ']';
+  }
+}
+
+
+function getTypesFrom_(block, name) {
+  var typeBlock = block.getInputTargetBlock(name);
+  var types;
+  if (!typeBlock || typeBlock.disabled) {
+    types = [];
+  } else if (typeBlock.type == 'type_other') {
+    types = [escapeString(typeBlock.getFieldValue('TYPE'))];
+  } else if (typeBlock.type == 'type_group') {
+    types = [];
+    for (var n = 0; n < typeBlock.typeCount_; n++) {
+      types = types.concat(getTypesFrom_(typeBlock, 'TYPE' + n));
+    }
+    // Remove duplicates.
+    var hash = Object.create(null);
+    for (var n = types.length - 1; n >= 0; n--) {
+      if (hash[types[n]]) {
+        types.splice(n, 1);
+      }
+      hash[types[n]] = true;
+    }
+  } else {
+    types = [escapeString(typeBlock.valueType)];
+  }
+  return types;
+}
+
+
+function injectCode(code, id) {
+  var pre = document.getElementById(id);
+  pre.textContent = code;
+  code = pre.innerHTML;
+  code = prettyPrintOne(code, 'js');
+  pre.innerHTML = code;
+}
+
+
+
+function getRootBlock() {
+  var blocks = workspaceRef.getTopBlocks(false);
+  for (var i = 0, block; block = blocks[i]; i++) {
+    if (block.type == 'factory_base') {
+      return block;
+    }
+  }
+  return null;
+}
+
+
   if(selectedFeature === 'Program your Arduino...'){
     return <PublicCanvas activity={activity} isSandbox={isSandbox}/>;
   }
@@ -368,18 +699,19 @@ export default function CustomBlock({ activity, isSandbox, workspace}) {
             <div id='newblockly-canvas'/>
             <Row id='block-bs'>{saveBlock('Save Block')}</Row>
             <Row id='pre-text'>Block Preview</Row>
-            <Row id='blocklyCanvasTop'>
+            <Row id='blocklyCanvasTop'  style={{ textAlign: 'left' }}>
               {/* Block Preview */}
+              {/* {preview} */}
             </Row>
             <Row id='def-text'>Block Definition</Row>
-            <Row id='blocklyCanvasMid'>
+            <Row id='blocklyCanvasMid'  style={{ textAlign: 'left' }}>
               {/* {Block Definition} */}
-              {blockCode}
+              {generatorCode}
             </Row>
             <Row id='gen-text'>Generator Stub</Row>
-            <Row id='blocklyCanvasBottom'>
+            <Row id='blocklyCanvasBottom'  style={{ textAlign: 'left' }}>
               {/* {Generator Stub} */}
-              {generatorCode}
+              {blockCode}
             </Row>
           </Spin>
         </div>
@@ -424,6 +756,9 @@ export default function CustomBlock({ activity, isSandbox, workspace}) {
       <block type="field_variable"></block>
       <block type="field_image"></block>
     </category>
+    <category name="Colour" id="colourCategory">
+      <block type="colour_hue"><mutation colour="20"></mutation><field name="HUE"></field></block>
+    </category>
     <category name="Type">
       <block type="type_group"></block>
       <block type="type_null"></block>
@@ -432,17 +767,6 @@ export default function CustomBlock({ activity, isSandbox, workspace}) {
       <block type="type_string"></block>
       <block type="type_list"></block>
       <block type="type_other"></block>
-    </category>
-    <category name="Colour" id="colourCategory">
-      <block type="colour_hue"><mutation colour="20"></mutation><field name="HUE">20</field></block>
-      <block type="colour_hue"><mutation colour="65"></mutation><field name="HUE">65</field></block>
-      <block type="colour_hue"><mutation colour="120"></mutation><field name="HUE">120</field></block>
-      <block type="colour_hue"><mutation colour="160"></mutation><field name="HUE">160</field></block>
-      <block type="colour_hue"><mutation colour="210"></mutation><field name="HUE">210</field></block>
-      <block type="colour_hue"><mutation colour="230"></mutation><field name="HUE">230</field></block>
-      <block type="colour_hue"><mutation colour="260"></mutation><field name="HUE">260</field></block>
-      <block type="colour_hue"><mutation colour="290"></mutation><field name="HUE">290</field></block>
-      <block type="colour_hue"><mutation colour="330"></mutation><field name="HUE">330</field></block>
     </category>
   </xml>
 
